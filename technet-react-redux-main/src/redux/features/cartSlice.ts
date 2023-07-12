79f507d28bc57f628651008181c120ba0c1268cf
rubayet-islam <season.ss2001@gmail.com>
2023-07-12 22:36:49 +0600

    addToCart

---
+++ technet-react-redux-main/src/redux/features/cartSlice.ts
@@ -0,0 +1,27 @@
+import { IProduct } from '@/types/globalTypes';
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface ICart {
+  products: IProduct[];
+  total: number;
+}
+
+const initialState: ICart = {
+  products: [],
+  total: 0,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<IProduct>) => {
+      state.products.push({ ...action.payload, quantity: 1 });
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
