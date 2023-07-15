05a57911a410f8fe9e0dd331f954775e777edb80
rubayet-islam <season.ss2001@gmail.com>
2023-07-15 12:51:12 +0600

    rtk query

---
+++ technet-react-redux-main/src/redux/api/apiSlice.ts
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  endpoints: (builder) => ({
+    //get all products query
+    getProducts: builder.query({
+      query: () => '/products',
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = api;
--- technet-react-redux-main/src/redux/store.ts
+++ technet-react-redux-main/src/redux/store.ts
@@ -1,11 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import cartReducer from './features/cartSlice';
 import productReducer from './features/productSlice';
+import { api } from './api/apiSlice';
+
 const store = configureStore({
   reducer: {
     cart: cartReducer,
     product: productReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
